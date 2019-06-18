49c80a5cda003004526de3e836dca4243967595b
MAriana <marianavasouza@gmail.com>
2019-06-18 17:58:13 -0300

    final version

---
+++ GoodWeather/Controllers/WeatherDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDetailsViewController.swift
+//  GoodWeather
+//
+//  Created by Smiles on 18/06/19.
+//  Copyright © 2019 Mariana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherDetailsViewController: UIViewController {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+}
